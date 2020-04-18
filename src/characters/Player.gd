2b16cf77d37729dd389af380f5d45066cbc0314d
Leandro Motta Barros <lmb@stackedboxes.org>
2020-04-18 15:10:04 -0300

    Nicer camera

--- src/characters/Player.gd
+++ src/characters/Player.gd
@@ -6,6 +6,9 @@ const TOP_SPEED = 300
 # Hit points. Player dies if <= 0.0.
 var _hp: float = 10.0
 
+# Current velocity
+var _velocity = Vector2(0, 0)
+
 onready var _bulletClass = preload("res://bullets/WeakBullet.tscn")
 
 func _process(_delta: float) -> void:
@@ -14,24 +17,28 @@ func _process(_delta: float) -> void:
 
 
 func _processMove() -> void:
-	var velocity := Vector2(
+	_velocity = Vector2(
 		Input.get_action_strength("moveRight") - Input.get_action_strength("moveLeft"),
 		Input.get_action_strength("moveDown") - Input.get_action_strength("moveUp"))
 
 	# If using keys, diagonals will be [1, 1]. Clamp to unit vector.
-	if velocity.length_squared() > 1.0:
-		velocity = velocity.normalized()
+	if _velocity.length_squared() > 1.0:
+		_velocity = _velocity.normalized()
 
 	# No need to multiply by delta (this is implied by move_and_slide)
-	velocity *= TOP_SPEED
+	_velocity *= TOP_SPEED
 
-	if velocity.length_squared() > 0:
-		rotation = velocity.angle()
+	if _velocity.length_squared() > 0:
+		rotation = _velocity.angle()
 
-	move_and_slide(velocity)
+	move_and_slide(_velocity)
 
 
 func _processFire() -> void:
 	if Input.is_action_just_pressed("fire"):
 		var bullet = _bulletClass.instance()
 		bullet.fire(self)
+
+
+func getSpeed() -> float:
+	return _velocity.length()
--- src/misc/PlayfieldCamera.gd
+++ src/misc/PlayfieldCamera.gd
@@ -1,1 +1,33 @@
 extends Camera2D
+
+const MIN_ZOOM := 1.2
+const MAX_ZOOM := 3.5
+
+const MIN_ZOOM_SPEED := 0
+const MAX_ZOOM_SPEED := 300
+
+const ZOOM_DECREASE_SPEED := 0.8
+const ZOOM_INCREASE_SPEED := ZOOM_DECREASE_SPEED * 3.0
+
+const ZOOM_EPSILON = 0.05
+
+var _targetZoom := MIN_ZOOM
+var _zoom := _targetZoom
+
+func _process(delta: float) -> void:
+	var speed = G.gs.player.getSpeed()
+
+	_targetZoom = lerp(MIN_ZOOM, MAX_ZOOM,
+		speed / (MAX_ZOOM_SPEED - MIN_ZOOM_SPEED))
+
+	_targetZoom = clamp(_targetZoom, MIN_ZOOM, MAX_ZOOM)
+
+	if _targetZoom < _zoom:
+		_zoom -= ZOOM_DECREASE_SPEED * delta
+	elif _targetZoom > _zoom:
+		_zoom += ZOOM_INCREASE_SPEED * delta
+
+	_zoom = clamp(_zoom, MIN_ZOOM, MAX_ZOOM)
+
+	if abs(_targetZoom - zoom.x) >= ZOOM_EPSILON:
+		zoom = Vector2(_zoom, _zoom)
